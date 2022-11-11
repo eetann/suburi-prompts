dcb0e7baba5a934c69df5985028d1fa850524a29
eetann <hideharu092@gmail.com>
2022-11-11 09:08:36 +0900

    add kleur example

---
+++ src/kleur-nested.js
@@ -0,0 +1,7 @@
+const {black, red, bgGreen, bgYellow, underline} = require("kleur")
+
+console.log(black("black"))
+console.log(red("red"))
+console.log(bgGreen("bgGreen"))
+console.log(bgYellow("bgYellow"))
+console.log(underline("underline"))
